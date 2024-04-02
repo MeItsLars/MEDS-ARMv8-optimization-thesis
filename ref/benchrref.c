f9cc3507a7ac1d32156bc55a41cd9542a0cd90c8
Lars Jeurissen <lars.jeurissen@ru.nl>
2024-04-02 15:22:20 +0200

    Add non-constant time version of test_pmod_mat_syst_nct() function

--- ref/benchrref.c
+++ ref/benchrref.c
@@ -141,6 +141,128 @@ int test_pmod_mat_syst_ct(pmod_mat_t *M, int M_r, int M_c)
   return ret;
 }
 
+int test_pmod_mat_syst_nct(pmod_mat_t *M, int M_r, int M_c)
+{
+  int ret = 0;
+
+  // Step 1: Forward elimination
+  for (int r = 0; r < M_r; r++)
+  {
+    // Step 2: Elimination
+    // In this loop, we eliminate the elements below the diagonal
+    // We do this by multiplying the pivot row by the inverse of the pivot element (the element on the diagonal)
+    // and then subtracting the result from the other rows
+
+    // Loop from the next row to the last row
+
+    // Get the element on the diagonal
+    uint64_t Mrr = pmod_mat_entry(M, M_r, M_c, r, r);
+    if (Mrr == 0)
+    {
+      for (int r2 = r + 1; r2 < M_r; r2++)
+      {
+        // If there is no point in adding the row, continue
+        if (pmod_mat_entry(M, M_r, M_c, r2, r) == 0)
+          continue;
+
+        // Otherwise, add the row and break
+        for (int c = r; c < M_c; c++)
+        {
+          uint64_t val = pmod_mat_entry(M, M_r, M_c, r2, c);
+          uint64_t Mrc = pmod_mat_entry(M, M_r, M_c, r, c);
+          pmod_mat_set_entry(M, M_r, M_c, r, c, (Mrc + val) % MEDS_p);
+        }
+        break;
+      }
+    }
+
+    uint64_t val = pmod_mat_entry(M, M_r, M_c, r, r);
+
+    // If, after everything, the element on the diagonal is (still) zero, return that we failed
+    // to convert this row in the matrix to RREF
+    if (val == 0)
+      return -1;
+
+    // Compute the multiplicative inverse of the element on the diagonal
+    val = GF_inv(val);
+
+    // normalize;
+    // multiply every element in the row with the multiplicative inverse of the element on the diagonal
+    // The goal of this is to make sure the element on the diagonal is exactly 1.
+    for (int c = r; c < M_c; c++)
+    {
+      uint64_t tmp = ((uint64_t)pmod_mat_entry(M, M_r, M_c, r, c) * val) % MEDS_p;
+      pmod_mat_set_entry(M, M_r, M_c, r, c, tmp);
+    }
+
+    // eliminate;
+    // For all rows below the current one, make sure the element in the current column is zero
+    // This is done by subtracting the current row from the other rows with the appropriate difference factor
+    // The goal is to prepare the next rows for the next iteration (making sure they have a zero in the columns left of the diagonal)
+    for (int r2 = r + 1; r2 < M_r; r2++)
+    {
+      // Calculate the factor
+      uint64_t factor = pmod_mat_entry(M, M_r, M_c, r2, r);
+
+      for (int c = r; c < M_c; c++)
+      {
+        // Calculate and set the new value
+        uint64_t tmp0 = pmod_mat_entry(M, M_r, M_c, r, c);
+        uint64_t tmp1 = pmod_mat_entry(M, M_r, M_c, r2, c);
+
+        uint64_t val = (tmp0 * factor) % MEDS_p;
+
+        val = (MEDS_p + tmp1 - val) % MEDS_p;
+
+        pmod_mat_set_entry(M, M_r, M_c, r2, c, val);
+      }
+    }
+  }
+
+  // At this point, the entire matrix is in RREF.
+
+  // back substitution;
+  // In this loop, we make sure the elements above the diagonal are zero
+  // We do this by subtracting the current row from the other rows with the appropriate difference factor
+  // The goal is to prepare the previous rows for the next iteration (making sure they have a zero in the columns right of the diagonal)
+
+  // Loop through the rows in reverse order (so we start with the last row which contains only one non-zero element)
+  for (int r = M_r - 1; r >= 0; r--)
+    // For every row above the current one, make sure the element in the current column is zero
+    for (int r2 = 0; r2 < r; r2++)
+    {
+      // Compute the factor
+      uint64_t factor = pmod_mat_entry(M, M_r, M_c, r2, r);
+
+      // Set the value in the current column
+      // >> This section seems to be unnecessary? Removing it still produces the same results
+      uint64_t tmp0 = pmod_mat_entry(M, M_r, M_c, r, r);
+      uint64_t tmp1 = pmod_mat_entry(M, M_r, M_c, r2, r);
+
+      uint64_t val = (tmp0 * factor) % MEDS_p;
+
+      val = (MEDS_p + tmp1 - val) % MEDS_p;
+
+      pmod_mat_set_entry(M, M_r, M_c, r2, r, val);
+      // <<
+
+      // Calculate and set the new values in columns right of the diagonal block.
+      // The values in the diagonal block are already 0 so we don't need to do anything with them.
+      for (int c = M_r; c < M_c; c++)
+      {
+        uint64_t tmp0 = pmod_mat_entry(M, M_r, M_c, r, c);
+        uint64_t tmp1 = pmod_mat_entry(M, M_r, M_c, r2, c);
+
+        uint64_t val = (tmp0 * factor) % MEDS_p;
+
+        val = (MEDS_p + tmp1 - val) % MEDS_p;
+
+        pmod_mat_set_entry(M, M_r, M_c, r2, c, val);
+      }
+    }
+  return ret;
+}
+
 uint16x4_t reduce(uint32x4_t red, uint16x4_t MEDS_p_16x4, uint32x4_t MEDS_p_32x4)
 {
   // >>> Reduce modulo MEDS_p
@@ -273,6 +395,11 @@ int test_pmod_mat_syst_ct_2(pmod_mat_t *M, int M_r, int M_c)
         uint16x4_t mask = vand_u16(vshr_n_u16(diff, 15), vdup_n_u16(1));
         val_16x4 = vadd_u16(vmul_u16(mask, val_16x4), vmul_u16(vsub_u16(vdup_n_u16(1), mask), diff));
 
+        // Reduce to a value between 0 and MEDS_p - 1: (constant time?)
+        // uint16x4_t cmp_res = vcge_u16(val_16x4, MEDS_p_16x4);
+        // uint16x4_t diff = vsub_u16(val_16x4, MEDS_p_16x4);
+        // val_16x4 = vbsl_u16(cmp_res, diff, val_16x4);
+
         // Store result
         vst1_u16(&M[(M_c)*r2 + c], val_16x4);
       }
@@ -375,6 +502,8 @@ int main(int argc, char *argv[])
         pmod_mat_set_entry(A1, A_ROWS, A_COLS, r, c, rand_el);
         pmod_mat_set_entry(A2, A_ROWS, A_COLS, r, c, rand_el);
       }
+    pmod_mat_set_entry(A1, A_ROWS, A_COLS, 4, 4, 0);
+    pmod_mat_set_entry(A2, A_ROWS, A_COLS, 4, 4, 0);
 
     // Run test A1
     long long old_rref_cc = -get_cyclecounter();
@@ -383,7 +512,7 @@ int main(int argc, char *argv[])
 
     // Run test A2
     long long new_rref_cc = -get_cyclecounter();
-    test_pmod_mat_syst_ct_2(A2, A_ROWS, A_COLS);
+    test_pmod_mat_syst_nct(A2, A_ROWS, A_COLS);
     new_rref_cc += get_cyclecounter();
 
     old_rref_cycles[round] = old_rref_cc;
