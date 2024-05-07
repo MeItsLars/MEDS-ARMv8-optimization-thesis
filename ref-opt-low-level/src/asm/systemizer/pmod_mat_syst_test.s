b56e3fb1406bdfb60fffab37f76e1e1e75b2121e
Lars Jeurissen <lars.jeurissen@ru.nl>
2024-05-07 15:33:42 +0200

    Working on asm systemizer

--- ref-opt-low-level/src/asm/systemizer/pmod_mat_syst_test.s
+++ ref-opt-low-level/src/asm/systemizer/pmod_mat_syst_test.s
@@ -2,44 +2,277 @@
 .arch armv8-a
 .global pmod_mat_syst_test
 pmod_mat_syst_test:
-    mov x3, x1
-    mov x5, #4093
-    dup v1.4h, w5
-    dup v2.4s, w5
-    dup v0.4h, wzr
-    mov x4, xzr
-    mov x6, #0
+    mov x1, #4
+    mov x3, #4093
+    dup v16.4h, w3
+    dup v17.8h, w3
+    dup v18.4s, w3
+    mov x2, xzr
+    mov x4, #0
 elimination_loop:
-    cmp x6, x3
+    cmp x4, #4
     b.eq elimination_loop_end
-    madd x9, x2, x6, x6
-    madd x10, x2, x6, x6
-    add x10, x0, x10, lsl #1
-    add x7, x6, #1
+    madd x7, x1, x4, x4
+    add x8, x0, x7, lsl #1
+    add x5, x4, #1
 elimination_row_zero_fix_outer_loop:
-    cmp x7, x1
+    cmp x5, 4
     b.eq elimination_row_zero_fix_outer_loop_end
-    madd x11, x2, x7, x6
-    add x11, x0, x11, lsl #1
-    ldrh w12, [x0, x9, lsl #1]
-    dup v3.4h, w12
-    cmeq v6.4h, v3.4h, v0.4h
-    mov x8, x6
-elimination_row_zero_fix_inner_loop_neon:
-    cmp x8, x2
-    b.eq elimination_row_zero_fix_inner_loop_neon_end
-    ld1 {v4.4h}, [x10]
-    ld1 {v5.4h}, [x11], #8
-    and v16.16b, v6.16b, v5.16b
-    st1 {v5.4h}, [x10], #8
-    add x8, x8, #1
-    b elimination_row_zero_fix_inner_loop_neon
-elimination_row_zero_fix_inner_loop_neon_end:
-    add x7, x7, #1
+    madd x9, x1, x5, x4
+    add x9, x0, x9, lsl #1
+    ldrh w10, [x0, x7, lsl #1]
+    dup v1.4h, w10
+    dup v2.8h, w10
+    cmeq v5.4h, v1.4h, #0
+    cmeq v6.8h, v2.8h, #0
+    mov x6, x4
+elimination_row_zero_fix_inner_loop_neon_16x8:
+    sub x14, x1, x6
+    cmp x14, #8
+    b.lt elimination_row_zero_fix_inner_loop_neon_16x4
+    ld1 {v3.8h}, [x8]
+    ld1 {v4.8h}, [x9], #16
+    and v19.16b, v6.16b, v4.16b
+    add v20.8h, v19.8h, v3.8h
+    cmhs v19.8h, v20.8h, v17.8h
+    and v19.16b, v19.16b, v17.16b
+    sub v20.8h, v20.8h, v19.8h
+    st1 {v20.8h}, [x8], #16
+    add x6, x6, #8
+    b elimination_row_zero_fix_inner_loop_neon_16x8
+elimination_row_zero_fix_inner_loop_neon_16x4:
+    sub x14, x1, x6
+    cmp x14, #4
+    b.lt elimination_row_zero_fix_inner_loop_scalar
+    ld1 {v3.4h}, [x8]
+    ld1 {v4.4h}, [x9], #8
+    and v19.16b, v5.16b, v4.16b
+    add v20.4h, v19.4h, v3.4h
+    cmhs v19.4h, v20.4h, v16.4h
+    and v19.16b, v19.16b, v16.16b
+    sub v20.4h, v20.4h, v19.4h
+    st1 {v20.4h}, [x8], #8
+    add x6, x6, #4
+elimination_row_zero_fix_inner_loop_scalar:
+    cmp x6, x1
+    b.ge elimination_row_zero_fix_inner_loop_end
+    ldrh w11, [x8]
+    ldrh w12, [x9], #2
+    cmp x10, #0
+    csel x14, x12, xzr, eq
+    add x14, x14, x11
+    cmp x14, #4093
+    csel x11, x3, xzr, ge
+    sub x14, x14, x11
+    strh w14, [x8], #2
+    add x6, x6, #1
+    b elimination_row_zero_fix_inner_loop_scalar
+elimination_row_zero_fix_inner_loop_end:
+    add x5, x5, #1
     b elimination_row_zero_fix_outer_loop
 elimination_row_zero_fix_outer_loop_end:
+    ldrh w10, [x0, x7, lsl #1]
+    cmp x10, #0
+    b.eq ret_fail
+    mul x14, x10, x10
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x15, x14, x10
+    lsr x6, x15, #12
+    mul x6, x6, x3
+    sub x15, x15, x6
+    lsr x6, x15, #12
+    mul x6, x6, x3
+    sub x15, x15, x6
+    mul x14, x15, x15
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x5, x14, x15
+    lsr x6, x5, #12
+    mul x6, x6, x3
+    sub x5, x5, x6
+    lsr x6, x5, #12
+    mul x6, x6, x3
+    sub x5, x5, x6
+    mul x14, x5, x5
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x5
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x15
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x14
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    mul x14, x14, x10
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    lsr x6, x14, #12
+    mul x6, x6, x3
+    sub x14, x14, x6
+    cmp x14, #4093
+    csel x6, x3, xzr, ge
+    sub x10, x14, x6
+    dup v1.4h, w10
+    add x8, x0, x7, lsl #1
+    mov x6, x4
+elimination_normalize_row_loop_neon_16x4:
+    sub x14, x1, x6
+    cmp x14, #4
+    b.lt elimination_normalize_row_loop_scalar
+    ld1 {v3.4h}, [x8]
+    umull v19.4s, v3.4h, v1.4h
+    ushr v20.4s, v19.4s, #12
+    mul v20.4s, v20.4s, v18.4s
+    sub v19.4s, v19.4s, v20.4s
+    ushr v20.4s, v19.4s, #12
+    mul v20.4s, v20.4s, v18.4s
+    sub v19.4s, v19.4s, v20.4s
+    sqxtn v3.4h, v19.4s
+    cmhs v20.4h, v3.4h, v16.4h
+    and v20.16b, v20.16b, v16.16b
+    sub v3.4h, v3.4h, v20.4h
+    st1 {v3.4h}, [x8], #8
+    add x6, x6, #4
+    b elimination_normalize_row_loop_neon_16x4
+elimination_normalize_row_loop_scalar:
+    cmp x6, x1
+    b.ge elimination_normalize_row_loop_end
+    ldrh w11, [x8]
+    mul x14, x11, x10
+    lsr x15, x14, #12
+    mul x15, x15, x3
+    sub x14, x14, x15
+    lsr x15, x14, #12
+    mul x15, x15, x3
+    sub x11, x14, x15
+    cmp x11, #4093
+    csel x15, x3, xzr, ge
+    sub x11, x11, x15
+    strh w11, [x8], #2
     add x6, x6, #1
+    b elimination_normalize_row_loop_scalar
+elimination_normalize_row_loop_end:
+    add x8, x0, x7, lsl #1
+    add x5, x4, #1
+elimination_eliminate_rows_loop:
+    cmp x5, 4
+    b.eq elimination_eliminate_rows_loop_end
+    madd x9, x1, x5, x4
+    add x9, x0, x9, lsl #1
+    ldrh w13, [x9]
+    dup v1.4h, w10
+    cmeq v5.4h, v1.4h, #0
+    mov x6, x4
+elimination_row_eliminate_inner_loop_scalar:
+    cmp x6, x1
+    b.ge elimination_row_eliminate_inner_loop_end
+    ldrh w11, [x8], #2
+    ldrh w12, [x9]
+    mul x14, x11, x13
+    lsr x15, x14, #12
+    mul x15, x15, x3
+    sub x14, x14, x15
+    lsr x15, x14, #12
+    mul x15, x15, x3
+    sub x14, x14, x15
+    cmp x14, #4093
+    csel x15, x3, xzr, ge
+    sub x14, x14, x15
+    add x15, x12, #4093
+    sub x15, x15, x14
+    cmp x15, #4093
+    csel x14, x3, xzr, ge
+    sub x12, x15, x14
+    strh w12, [x9], #2
+    add x6, x6, #1
+    b elimination_row_eliminate_inner_loop_scalar
+elimination_row_eliminate_inner_loop_end:
+    add x5, x5, #1
+    b elimination_eliminate_rows_loop
+elimination_eliminate_rows_loop_end:
+    add x4, x4, #1
     b elimination_loop
 elimination_loop_end:
-    mov x0, x4
+ret_success:
+    mov x0, x2
+    ret
+ret_fail:
+    mov x0, #-1
     ret
--- ref-opt-low-level/src/benchsystemizer.c
+++ ref-opt-low-level/src/benchsystemizer.c
@@ -68,7 +68,6 @@ int test_pmod_mat_syst_ct_partial_swap_backsub(pmod_mat_t *M, int M_r, int M_c, 
       }
     }
 
-    /*
     uint64_t val = pmod_mat_entry(M, M_r, M_c, r, r);
 
     if (val == 0)
@@ -99,7 +98,7 @@ int test_pmod_mat_syst_ct_partial_swap_backsub(pmod_mat_t *M, int M_r, int M_c, 
 
         pmod_mat_set_entry(M, M_r, M_c, r2, c, val);
       }
-    }*/
+    }
   }
 
   return ret;
@@ -141,7 +140,179 @@ int test_pmod_mat_syst_ct_partial_swap_backsub(pmod_mat_t *M, int M_r, int M_c, 
   return ret;*/
 }
 
-extern int pmod_mat_syst_test(pmod_mat_t *M, int M_r, int M_c);
+uint16x4_t reduce(uint32x4_t red, uint16x4_t MEDS_p_16x4, uint32x4_t MEDS_p_32x4)
+{
+  // >>> Reduce modulo MEDS_p
+  // Apply two reductions
+  uint32x4_t tmp = vshrq_n_u32(red, GFq_bits);
+  tmp = vmulq_n_u32(tmp, MEDS_p);
+  red = vsubq_u32(red, tmp);
+  tmp = vshrq_n_u32(red, GFq_bits);
+  tmp = vmulq_n_u32(tmp, MEDS_p);
+  red = vsubq_u32(red, tmp);
+
+  // Reduce to a value between 0 and MEDS_p - 1:
+  uint32x4_t diff = vsubq_u32(red, MEDS_p_32x4);
+  uint32x4_t mask = vandq_u32(vshrq_n_u32(diff, 31), vdupq_n_u32(1));
+  red = vaddq_u32(vmulq_u32(mask, red), vmulq_u32(vsubq_u32(vdupq_n_u32(1), mask), diff));
+
+  // Convert to smaller type
+  return vqmovn_u32(red);
+}
+
+int test_pmod_mat_syst_ct_partial_swap_backsub_2(pmod_mat_t *M, int M_r, int M_c)
+{
+  int ret = 0;
+
+  uint16x8_t MEDS_p_16x8 = vdupq_n_u16(MEDS_p);
+  uint16x4_t MEDS_p_16x4 = vdup_n_u16(MEDS_p);
+  uint32x4_t MEDS_p_32x4 = vdupq_n_u32(MEDS_p);
+  uint32x4_t one_32x4 = vdupq_n_u32(1);
+
+  for (int r = 0; r < M_r; r++)
+  {
+    for (int r2 = r + 1; r2 < M_r; r2++)
+    {
+      uint64_t Mrr = pmod_mat_entry(M, M_r, M_c, r, r);
+      uint16x8_t swap = vdupq_n_u16(GFq_eq0(Mrr));
+
+      int c;
+      for (c = r; c < M_c - 8; c += 8)
+      {
+        // Compute result
+        uint16x8_t val = vld1q_u16(&M[(M_c)*r2 + c]);
+        uint16x8_t Mrc = vld1q_u16(&M[(M_c)*r + c]);
+        uint16x8_t result = vaddq_u16(Mrc, vmulq_u16(val, swap));
+
+        // Subtract MEDS_p if result is larger than MEDS_p
+        uint16x8_t mask = vcgtq_u16(result, MEDS_p_16x8);
+        result = vsubq_u16(result, vandq_u16(MEDS_p_16x8, mask));
+
+        // Store result
+        vst1q_u16(&M[(M_c)*r + c], result);
+      }
+      for (c = c; c < M_c; c++)
+      {
+        uint64_t val = pmod_mat_entry(M, M_r, M_c, r2, c);
+        uint64_t Mrc = pmod_mat_entry(M, M_r, M_c, r, c);
+        pmod_mat_set_entry(M, M_r, M_c, r, c, (Mrc + val * GFq_eq0(Mrr)) % MEDS_p);
+      }
+    }
+
+    uint64_t val = pmod_mat_entry(M, M_r, M_c, r, r);
+
+    if (val == 0)
+      return -1;
+
+    // Compute the multiplicative inverse of the element on the diagonal
+    val = GF_inv(val);
+    uint16x4_t val_v = vdup_n_u16(val);
+
+    // normalize;
+    int c;
+    for (c = r; c < M_c - 4; c += 4)
+    {
+      // Compute result
+      uint16x4_t vals = vld1_u16(&M[(M_c)*r + c]);
+      uint32x4_t red = vmull_u16(vals, val_v);
+      uint16x4_t red_16x4 = reduce(red, MEDS_p_16x4, MEDS_p_32x4);
+
+      // Store result
+      vst1_u16(&M[(M_c)*r + c], red_16x4);
+    }
+    for (c = c; c < M_c; c++)
+    {
+      uint64_t tmp = ((uint64_t)pmod_mat_entry(M, M_r, M_c, r, c) * val) % MEDS_p;
+      pmod_mat_set_entry(M, M_r, M_c, r, c, tmp);
+    }
+
+    // eliminate;
+    for (int r2 = r + 1; r2 < M_r; r2++)
+    {
+      // Calculate the factor
+      GFq_t factor = pmod_mat_entry(M, M_r, M_c, r2, r);
+      uint16x4_t factor_16x4 = vdup_n_u16(factor);
+
+      for (int c = r; c < M_c; c += 4)
+      {
+        // First, compute val (red)
+        uint16x4_t tmp0 = vld1_u16(&M[(M_c)*r + c]);
+        uint32x4_t val = vmull_u16(tmp0, factor_16x4);
+        uint16x4_t val_16x4 = reduce(val, MEDS_p_16x4, MEDS_p_32x4);
+
+        // Compute the new val
+        uint16x4_t tmp1 = vld1_u16(&M[(M_c)*r2 + c]);
+        tmp1 = vadd_u16(tmp1, MEDS_p_16x4);
+        val_16x4 = vsub_u16(tmp1, val_16x4);
+
+        // Reduce to a value between 0 and MEDS_p - 1:
+        uint16x4_t diff = vsub_u16(val_16x4, MEDS_p_16x4);
+        uint16x4_t mask = vand_u16(vshr_n_u16(diff, 15), vdup_n_u16(1));
+        val_16x4 = vadd_u16(vmul_u16(mask, val_16x4), vmul_u16(vsub_u16(vdup_n_u16(1), mask), diff));
+
+        // Store result
+        vst1_u16(&M[(M_c)*r2 + c], val_16x4);
+      }
+    }
+  }
+
+  /*
+  // back substitution;
+  for (int r = M_r - 1; r >= 0; r--)
+    for (int r2 = 0; r2 < r; r2++)
+    {
+      // Compute the factor
+      uint64_t factor = pmod_mat_entry(M, M_r, M_c, r2, r);
+      uint16x4_t factor_16x4 = vdup_n_u16(factor);
+
+      // Set the value in the current column
+      // >> This section seems to be unnecessary? Removing it still produces the same results
+      uint64_t tmp0 = pmod_mat_entry(M, M_r, M_c, r, r);
+      uint64_t tmp1 = pmod_mat_entry(M, M_r, M_c, r2, r);
+
+      uint64_t val = (tmp0 * factor) % MEDS_p;
+
+      val = (MEDS_p + tmp1 - val) % MEDS_p;
+
+      pmod_mat_set_entry(M, M_r, M_c, r2, r, val);
+      // <<
+
+      // Calculate and set the new values in columns right of the diagonal block.
+      // The values in the diagonal block are already 0 so we don't need to do anything with them.
+      for (int c = M_r; c < M_c; c += 4)
+      {
+        // uint64_t tmp0 = pmod_mat_entry(M, M_r, M_c, r, c);
+        // uint64_t tmp1 = pmod_mat_entry(M, M_r, M_c, r2, c);
+
+        // uint64_t val = (tmp0 * factor) % MEDS_p;
+
+        // val = (MEDS_p + tmp1 - val) % MEDS_p;
+
+        // pmod_mat_set_entry(M, M_r, M_c, r2, c, val);
+
+        // First, compute val (red)
+        uint16x4_t tmp0 = vld1_u16(&M[(M_c)*r + c]);
+        uint32x4_t val = vmull_u16(tmp0, factor_16x4);
+        uint16x4_t val_16x4 = reduce(val, MEDS_p_16x4, MEDS_p_32x4);
+
+        // Compute the new val
+        uint16x4_t tmp1 = vld1_u16(&M[(M_c)*r2 + c]);
+        tmp1 = vadd_u16(tmp1, MEDS_p_16x4);
+        val_16x4 = vsub_u16(tmp1, val_16x4);
+
+        // Reduce to a value between 0 and MEDS_p - 1:
+        uint16x4_t diff = vsub_u16(val_16x4, MEDS_p_16x4);
+        uint16x4_t mask = vand_u16(vshr_n_u16(diff, 15), vdup_n_u16(1));
+        val_16x4 = vadd_u16(vmul_u16(mask, val_16x4), vmul_u16(vsub_u16(vdup_n_u16(1), mask), diff));
+
+        // Store result
+        vst1_u16(&M[(M_c)*r2 + c], val_16x4);
+      }
+    }*/
+  return ret;
+}
+
+extern int pmod_mat_syst_test(pmod_mat_t *M);
 
 #define A_ROWS 4
 #define A_COLS 4
@@ -159,11 +330,13 @@ int main(int argc, char *argv[])
 
   __attribute__((aligned(16))) pmod_mat_t A1[A_ROWS * A_COLS];
   __attribute__((aligned(16))) pmod_mat_t A2[A_ROWS * A_COLS];
+  __attribute__((aligned(16))) pmod_mat_t A3[A_ROWS * A_COLS];
 
   keccak_state shake;
   shake256_absorb_once(&shake, seed1, MEDS_pub_seed_bytes);
 
   long long old_systemizer_cycles[ROUNDS];
+  long long intrinsic_systemizer_cycles[ROUNDS];
   long long new_systemizer_cycles[ROUNDS];
 
   // Fill matrices with random values
@@ -173,9 +346,11 @@ int main(int argc, char *argv[])
       GFq_t rand_el = rnd_GF(&shake);
       pmod_mat_set_entry(A1, A_ROWS, A_COLS, r, c, rand_el);
       pmod_mat_set_entry(A2, A_ROWS, A_COLS, r, c, rand_el);
+      pmod_mat_set_entry(A3, A_ROWS, A_COLS, r, c, rand_el);
     }
   pmod_mat_set_entry(A1, A_ROWS, A_COLS, 0, 0, 0);
   pmod_mat_set_entry(A2, A_ROWS, A_COLS, 0, 0, 0);
+  pmod_mat_set_entry(A3, A_ROWS, A_COLS, 0, 0, 0);
 
   printf("A:\n");
   pmod_mat_fprint(stdout, A1, A_ROWS, A_COLS);
@@ -189,8 +364,15 @@ int main(int argc, char *argv[])
 
   for (int round = 0; round < ROUNDS - 1; round++)
   {
+    intrinsic_systemizer_cycles[round] = get_cyclecounter();
+    test_pmod_mat_syst_ct_partial_swap_backsub_2(A3, A_ROWS, A_COLS);
+  }
+  intrinsic_systemizer_cycles[ROUNDS - 1] = get_cyclecounter();
+
+  for (int round = 0; round < ROUNDS - 1; round++)
+  {
     new_systemizer_cycles[round] = get_cyclecounter();
-    pmod_mat_syst_test(A2, A_ROWS, A_COLS);
+    pmod_mat_syst_test(A2);
   }
   new_systemizer_cycles[ROUNDS - 1] = get_cyclecounter();
 
@@ -200,15 +382,18 @@ int main(int argc, char *argv[])
   pmod_mat_fprint(stdout, A2, A_ROWS, A_COLS);
 
   double old_systemizer_median_cc = median_2(old_systemizer_cycles, ROUNDS, 0);
+  double intrinsic_systemizer_median_cc = median_2(intrinsic_systemizer_cycles, ROUNDS, 0);
   double new_systemizer_median_cc = median_2(new_systemizer_cycles, ROUNDS, 0);
 
   // Print results
   double percentage = new_systemizer_median_cc / old_systemizer_median_cc * 100;
   double improvement = (new_systemizer_median_cc - old_systemizer_median_cc) / old_systemizer_median_cc * 100;
+  double improvement_intrinsics = (new_systemizer_median_cc - intrinsic_systemizer_median_cc) / intrinsic_systemizer_median_cc * 100;
   printf("Old median: %f\n", old_systemizer_median_cc);
   printf("New median: %f\n", new_systemizer_median_cc);
   printf("Percentage: %f%%\n", percentage);
   printf("Improvement: %f%%\n", improvement);
+  printf("Improvement (intrinsics): %f%%\n", improvement_intrinsics);
 
   disable_cyclecounter();
 
